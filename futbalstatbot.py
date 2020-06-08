006660e1660e3c8fa4f952c833ce16eee65a1af2
Sphairat <40624766+sphairat@users.noreply.github.com>
2020-06-08 23:52:01 +0400

    Update futbalstatbot.py

--- futbalstatbot.py
+++ futbalstatbot.py
@@ -6,7 +6,7 @@ def sms(bot, update):
     bot.message.reply_text('Привет, {}! Я футбольный бот! \nЯ пока ни хрена не умею! Но скоро научусь! \nЯ очень на это надеюсь, ' .format(bot.message.chat.first_name))
 
 def main():
-    my_bot = Updater("1163071581:AAFE-lN_YbFxHsZMnJoN3Ht7qKC7pXb4-Q0", "https://telegg.ru/orig/bot", use_context=True)
+    my_bot = Updater("TOKEN", "https://telegg.ru/orig/bot", use_context=True)
     my_bot.dispatcher.add_handler(CommandHandler('start',sms))
     my_bot.start_polling()
     my_bot.idle()
