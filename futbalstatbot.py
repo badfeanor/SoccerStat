ade25f0de37ac3c41185886a89e09046c5250e8b
Sphairat <sharonoff.mitek@gmail.com>
2020-06-10 14:44:48 +0400

    add buttons for 4 leagues

--- futbalstatbot.py
+++ futbalstatbot.py
@@ -1,13 +1,39 @@
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
 def sms(bot, update):
     print('Кто-то написал /start. Что мне делать?')
-    bot.message.reply_text('Привет, {}! Я футбольный бот! \nЯ пока ни хрена не умею! Но скоро научусь! \nЯ очень на это надеюсь, ' .format(bot.message.chat.first_name))
+    my_keyboard = ReplyKeyboardMarkup([['/england', '/italy'], ['/spain', '/germany']]) #добавление кнопок
+    bot.message.reply_text('Привет, я футбольный бот! \nЯ пока ни хрена не умею! Но скоро научусь! \nЯ очень на это надеюсь, {}! \nА пока выбери какой чемпионат тебе интереснее....' .format(bot.message.chat.first_name), reply_markup=my_keyboard)
+
+def eng(bot, update):
+    print('Кто-то хочет Англию')
+    my_keyboard = ReplyKeyboardMarkup([['/england', '/italy'], ['/spain', '/germany']]) #добавление кнопок
+    bot.message.reply_text('ММММ! \n{}, да ты ценитель футбола с туманного Альбиона! Тогда лови. \n\n https://www.soccer.ru/tournament/england/table' .format(bot.message.chat.first_name), reply_markup=my_keyboard)
+
+def ger(bot, update):
+    print('Кто-то хочет Германию')
+    my_keyboard = ReplyKeyboardMarkup([['/england', '/italy'], ['/spain', '/germany']])  # добавление кнопок
+    bot.message.reply_text('ОО! Я-я! \nА ты, {}, любитель брутального арийского фузбала!!! \n\n https://www.soccer.ru/tournament/germany/table' .format(bot.message.chat.first_name), reply_markup=my_keyboard)
+
+def esp(bot, update):
+    print('Кто-то хочет Испанию')
+    my_keyboard = ReplyKeyboardMarkup([['/england', '/italy'], ['/spain', '/germany']])  # добавление кнопок
+    bot.message.reply_text('Как оказалось, {}, ты у нас ещё один ценитель корриды и загорелых испанских мулаток! Тогда лови! \n\n https://www.soccer.ru/tournament/spain/table' .format(bot.message.chat.first_name), reply_markup=my_keyboard)
+
+def ita(bot, update):
+    print('Кто-то хочет Италию')
+    my_keyboard = ReplyKeyboardMarkup([['/england', '/italy'], ['/spain', '/germany']])  # добавление кнопок
+    bot.message.reply_text('Ох и не ожидал от тебя я такого, {}! \nЛюбуйся на здоровье \n\n https://www.soccer.ru/tournament/italy/table' .format(bot.message.chat.first_name), reply_markup=my_keyboard)
 
 def main():
     my_bot = Updater("TOKEN", "https://telegg.ru/orig/bot", use_context=True)
-    my_bot.dispatcher.add_handler(CommandHandler('start',sms))
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
+    my_bot.dispatcher.add_handler(CommandHandler('england', eng))
+    my_bot.dispatcher.add_handler(CommandHandler('germany', ger))
+    my_bot.dispatcher.add_handler(CommandHandler('spain', esp))
+    my_bot.dispatcher.add_handler(CommandHandler('italy', ita))
     my_bot.start_polling()
     my_bot.idle()
 
