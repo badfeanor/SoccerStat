81c441eebc1cb780c597eae7764c316f41ee5b53
root <root@nibiru.bitray.ru>
2020-08-08 13:37:53 +0300

    bugfix new parser and add team_id

--- parser_new.py
+++ parser_new.py
@@ -12,7 +12,7 @@ conn = psycopg2.connect(dbname='soccer_stat', user='soccer', password=dbpass, ho
 ligasFetch = conn.cursor()
 ligasFetch.execute("select * from metadata.parser_info;")
 liga_dates = ligasFetch.fetchall()
-conn.close()
+
 for i in liga_dates:
     globals()[i[0]] = i[1]
 
@@ -27,15 +27,26 @@ for liga in ligas_names:
     table = soup.body.find('table', attrs={'class': 'stat-table table'})
     rows = table.find_all("tr")
     data = []
+    team = 'NO TEAM'
     for row in rows:
         cols = row.find_all("td")
         for col in cols:
             for a in col.findAll("a", href=True):
                 team = str(a['href'])[22:-1].replace('-', '_')
+                print(team)
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
-        data.append(team)
+        team_stat = []
+        for ele in cols:
+    	    team_stat.append(ele)
+        team_stat.append(team)
+        data.append(team_stat)
     del data[0]
+    print (data)
+
+    InsData = conn.cursor()
+    InsData.execute("TRUNCATE table " + liga + ".champ_stat;")
+    InsData.executemany("INSERT INTO " + liga + ".champ_stat VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", data)
+    conn.commit()
 
     table_pretty = PrettyTable()
     table_pretty.field_names = ["#", "Команда", "И", "В", "Н", "П", "МЗ", "МП", "О"]
@@ -48,9 +59,4 @@ for liga in ligas_names:
     options = {'width': 385, 'disable-smart-width': '', 'encoding': "UTF-8", 'format': 'png'}
     imgkit.from_string(html, '/opt/SoccerStat_metadata/' + liga + '/champ_stat.png', options=options, css=css)
 
-    InsData = conn.cursor()
-    InsData.execute("CREATE TABLE IF NOT EXISTS " + liga + ".champ_stat;");
-    InsData.execute("TRUNCATE table " + liga + ".champ_stat;")
-    InsData.executemany("INSERT INTO " + liga + ".champ_stat VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", data)
-    conn.commit()
 conn.close()
